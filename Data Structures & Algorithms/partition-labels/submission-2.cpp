e7d8b37a020a39c7e0b7d3b91e157223c19af04d
Zaid <157042355+zaidhanif1@users.noreply.github.com>
2026-06-04 22:11:37 -0400

    Add: partition-labels - submission-2

---
+++ Data Structures & Algorithms/partition-labels/submission-2.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) 
+    {
+        std::unordered_map<char, int> freq;
+        std::vector<int> res;
+        for (char c : s)
+        {
+            freq[c]++;
+        }
+
+        //okay so what we'll do is have a count begin the first time we see a character
+        int curr_count = 0;
+        int curr_freq = 0;
+        std::set<int> seen;
+        for (char c : s)
+        {
+
+            if (!seen.contains(c))
+            {
+                curr_freq += freq[c]; //account for seeing the character
+                seen.insert(c);
+            }
+            curr_freq--;
+            curr_count++;
+            if (!curr_freq)
+            {
+                res.push_back(curr_count);
+                curr_count = 0;
+            }
+
+        }
+        return res;
+        
+    }
+};
